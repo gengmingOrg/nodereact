5862bf985d986196b984f3bdb8a893918c85e872
gengming <1154445357@qq.com>
2018-05-05 16:58:40 +0800

    改版

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 module.exports = {
     entry: path.resolve(__dirname, '../src/index.js'), //指定入口文件，程序从这里开始编译,__dirname当前所在目录, ../表示上一级目录, ./同级目录
     output: {
@@ -9,12 +10,7 @@ module.exports = {
         rules: [
             {
                 test: /\.(js|jsx)$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['es2015', 'react'],
-                    }
-                },
+                loader: 'babel-loader',
                 exclude: /node_modules/
             }
         ]
